679854b0af7d38478d4f8f80366515cac84822c9
faranbutt <66089079+faranbutt@users.noreply.github.com>
2020-06-03 21:18:05 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit <code>src/App.js</code> and save to reload.
+        <p>Created By Faran Butt
         </p>
         <a
           className="App-link"
